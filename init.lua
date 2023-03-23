a9ccd986edd1020381a7d94bf84dc43c67287520
Romain Commandé <romain.commande@wttj.co>
2023-03-24 00:45:53 +0100

    feat: replace bufferline by cokeline

--- init.lua
+++ init.lua
@@ -258,15 +258,26 @@ plugins = {
 		end,
 		cmd = { "NeoTreeToggle", "NeoTreeFocusToggle" },
 	},
-	-- {
-	-- 	"akinsho/bufferline.nvim",
-	-- 	config = function()
-	-- 		require("bufferline").setup()
-	-- 		vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCyclePrev<cr>", { noremap = true })
-	-- 		vim.keymap.set("n", "<backspace>", "<cmd>BufferLineCycleNext<cr>",
-	-- 			{ noremap = true })
-	-- 	end
-	-- },
+	{
+		'willothy/nvim-cokeline',
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- If you want devicons
+		config = function()
+			require('cokeline').setup()
+
+			vim.keymap.set('n', '<Tab>', '<Plug>(cokeline-focus-prev)', { silent = true })
+			vim.keymap.set('n', '<backspace>', '<Plug>(cokeline-focus-next)', { silent = true })
+			vim.keymap.set('n', '<Leader>p', '<Plug>(cokeline-switch-prev)', { silent = true })
+			vim.keymap.set('n', '<Leader>n', '<Plug>(cokeline-switch-next)', { silent = true })
+			vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { silent = true })
+
+			for i = 1, 9 do
+				vim.keymap.set('n', ('<F%s>'):format(i), ('<Plug>(cokeline-focus-%s)'):format(i),
+					{ silent = true })
+				vim.keymap.set('n', ('<Leader>%s'):format(i), ('<Plug>(cokeline-switch-%s)'):format(i),
+					{ silent = true })
+			end
+		end
+	},
 	{
 		"edluffy/hologram.nvim",
 		config = function()
@@ -298,20 +309,6 @@ plugins = {
 				-- or leave it empty to use the default settings
 				-- refer to the configuration section below
 			}
-		end
-	},
-	{
-		'willothy/nvim-cokeline',
-		requires = 'kyazdani42/nvim-web-devicons', -- If you want devicons
-		config = function()
-			require('cokeline').setup()
-			-- vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCyclePrev<cr>", { noremap = true })
-			-- vim.keymap.set("n", "<backspace>", "<cmd>BufferLineCycleNext<cr>",
-
-			vim.keymap.set('n', '<Tab>', '<Plug>(cokeline-focus-prev)', { silent = true })
-			vim.keymap.set('n', '<backspace>', '<Plug>(cokeline-focus-next)', { silent = true })
-			vim.keymap.set('n', '<Leader>p', '<Plug>(cokeline-switch-prev)', { silent = true })
-			vim.keymap.set('n', '<Leader>n', '<Plug>(cokeline-switch-next)', { silent = true })
 		end
 	},
 
